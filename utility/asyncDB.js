076406d208f09172debf80c294b58ab8dc9bfe7f
haohao0908 <n1056444@ntub.edu.tw>
2019-09-22 23:00:59 +0800

    MyApp

--- utility/asyncDB.js
+++ utility/asyncDB.js
@@ -8,7 +8,7 @@ const {Client} = require('pg');
 //-----------------------
 // 自己的資料庫連結位址
 //-----------------------
-var pgConn = 'postgres://blgwsyskuyxazi:59788010e5b634281ca8dafa0e824635aba960fa3ae22af0deac4f916d9367be@ec2-54-197-239-115.compute-1.amazonaws.com:5432/dc0gchj9ibuh72';
+var pgConn = 'postgres://mzukkxyyafifqy:bb0667ca834a71be9f9d62c8d2e34ffa6fd5f71d24c1e73dc9577a679de7b379@ec2-107-20-173-2.compute-1.amazonaws.com:5432/dccrc717t7kg91';
 
 //產生可同步執行query物件的函式
 function query(sql, value=null) {
