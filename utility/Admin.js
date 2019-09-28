0f60e4b4ffe01c2c6a38c6d703fba6481231b1be
haohao0908 <N1056444@ntub.edu.tw>
2019-09-28 17:39:57 +0800

    myApp

--- utility/Admin.js
+++ utility/Admin.js
@@ -47,25 +47,25 @@ var SelectSaveUser = async function(id,name){
 //------------------------------------------
 // 撈取要推播的資訊
 //------------------------------------------
-var AdminMessengePushJdge = async function(id){
-    //存放結果
-    let result;  
-    //讀取資料庫
-    await query('SELECT mem.user_id,admin.adminpush_content,admin.adminpush_enddate FROM teammember as mem INNER JOIN adminpush as admin ON (admin.project_id=mem.project_id)INNER JOIN savelineuser as line ON (mem.user_id=$1)', [id])
-        .then((data) => {
-            if(data.rows.length > 0){
-                result = data.rows;  //學生資料(物件)
-            }else{
-                result = -1;  //找不到資料
-            }    
-        }, (error) => {
-            result = -9;  //執行錯誤
-        });
-        console.log('admin結果');
-        console.log(result);
-    //回傳執行結果
-        return result;  
-}
+// var AdminMessengePushJdge = async function(id){
+//     //存放結果
+//     let result;  
+//     //讀取資料庫
+//     await query('SELECT mem.user_id,admin.adminpush_content,admin.adminpush_enddate FROM teammember as mem INNER JOIN adminpush as admin ON (admin.project_id=mem.project_id)INNER JOIN savelineuser as line ON (mem.user_id=$1)', [id])
+//         .then((data) => {
+//             if(data.rows.length > 0){
+//                 result = data.rows;  //學生資料(物件)
+//             }else{
+//                 result = -1;  //找不到資料
+//             }    
+//         }, (error) => {
+//             result = -9;  //執行錯誤
+//         });
+//         console.log('admin結果');
+//         console.log(result);
+//     //回傳執行結果
+//         return result;  
+// }
 //------------------------------------------
 //匯出
 module.exports = {SaveUser,AdminMessengePushJdge,SelectSaveUser};
