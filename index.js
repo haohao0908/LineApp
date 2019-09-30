71b358b729a8e8e5f5fefc138485de00f9f7e85f
haohao0908 <N1056444@ntub.edu.tw>
2019-09-30 09:18:33 +0800

    test

--- index.js
+++ index.js
@@ -64,112 +64,112 @@ bot.on('unfollow', function (event) {
 //--------------------------------
 // 查詢全部id
 //--------------------------------
-SelectUser();
-var timer;
-function SelectUser() {
-    clearTimeout(timer);
-    Admin.SelectSaveUser().then(data => {
-        var allUsers = [];
-        if (data == -1) {
-            event.reply('找不到資料');
-        } else if (data == -9) {
-            event.reply('執行錯誤');
-        } else {
-            data.forEach(item => {
-                allUsers.push(item.userid);
-            });
-        }
-        if (allUsers != []) {
-            PushMsg(allUsers);
-        }
-    });
-    timer = setInterval(SelectUser, 60000);
-}
-//--------------------------------
-// 推送訊息
-//--------------------------------
-function PushMsg(id) {
-    let allUsers = id;
-    for (var i = 0; i < allUsers.length; i++) {
-        Admin.AdminMessengePushJdge(allUsers[i]).then(data => {
-            if (data == -1) {
-                console.log('觸發-1');
-            }
-            else if (data == -9) {
-                console.log('處發-9');
-            }
-            else {
-                console.log('foreach');
-                data.forEach(item => {
-                    //當下時間＃＃
-                    var DateTime = new Date();
-                    CurrentTime(DateTime);
-                    //處理newDate()時間格式
-                    function CurrentTime(strDate) {
-                        var date = new Date(strDate);
-                        var y = date.getFullYear();
-                        console.log('y');
-                        console.log(y);
-                        var m = date.getMonth() + 1;
-                        m = m < 10 ? ('0' + m) : m;
-                        console.log('m');
-                        console.log(m);
-                        var d = date.getDate();
-                        d = d < 10 ? ('0' + d) : d;
-                        console.log('d');
-                        console.log(d);
-                        var h = date.getHours();
-                        h = h < 10 ? ('0' + h) : h;
-                        var minute = date.getMinutes();
-                        minute = minute < 10 ? ('0' + minute) : minute;
-                        var s = date.getSeconds();
-                        s = s < 10 ? ('0' + s) : s;
-                        var str = y + "-" + m + "-" + d + "T" + h + ":" + minute + ":" + s + '.000Z';
-                        console.log('str'+str)
-                        timeFn(item.adminpush_enddate, str)
-                    };
-                    //判斷是否在到期3小時內，每1小時推播一次
-                    function timeFn(d1, CurrentTime) {//傳入處理好的時間
-                        var dateBegin = new Date(d1);//傳入參數
-                        var dateEnd = new Date(CurrentTime);
-                        console.log('判斷相差時間');
-                        console.log(dateBegin);
-                        console.log(dateEnd);
-                        var dateDiff = dateBegin.getTime() - dateEnd.getTime();//时间差的毫秒數
-                        console.log('dateDiff'+dateDiff);
-                        var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天數
-                        console.log('dayDiff'+dayDiff);
-                        var leave1 = dateDiff % (24 * 3600 * 1000)    //计算天數後剩餘的毫秒數
-                        console.log('leave1'+leave1);
-                        var hours = Math.floor(leave1 / (3600 * 1000))//计算出小時數
-                        console.log('hours'+hours);
-                        //计算相差分鐘數
-                        var leave2 = leave1 % (3600 * 1000)    //计算小时數後剩餘毫秒數
-                        var minutes = Math.floor(leave2 / (60 * 1000))//计算相差分鐘數
-                        //计算相差秒數
-                        var leave3 = leave2 % (60 * 1000)      //计算分鐘數後剩餘毫秒數
-                        var seconds = Math.round(leave3 / 1000)
-                        console.log(" 相差 " + dayDiff + "天 " + hours + "小時" + minutes + "分鐘" + seconds + " 秒")
-                        // if (hours < 3 && hours >= 0) {
-                        //     console.log('進行推播')
-                        //     BotPushMsg()
-                        //     var timer2;
-                        //     function BotPushMsg(){
-                        //         clearTimeout(timer2);
-                        //         bot.push(item.user_id,'組長說：'+item.adminpush_content+'\n'+'到期時間'+item.adminpush_enddate);
-                        //     }
-                        //     timer2 = setInterval(BotPushMsg, 1000*60*60);
-                        // }
-                        console.log(" 相差 " + dayDiff + "天 " + hours + "小時" + minutes + "分鐘" + seconds + " 秒")
-                    }
-                    // bot.push(item.user_id,'組長說：'+item.adminpush_content+'\n'+'到期時間'+item.adminpush_enddate);
-                })
-            }
-        })
-    }
-}
+// SelectUser();
+// var timer;
+// function SelectUser() {
+//     clearTimeout(timer);
+//     Admin.SelectSaveUser().then(data => {
+//         var allUsers = [];
+//         if (data == -1) {
+//             event.reply('找不到資料');
+//         } else if (data == -9) {
+//             event.reply('執行錯誤');
+//         } else {
+//             data.forEach(item => {
+//                 allUsers.push(item.userid);
+//             });
+//         }
+//         if (allUsers != []) {
+//             PushMsg(allUsers);
+//         }
+//     });
+//     timer = setInterval(SelectUser, 60000);
+// }
+// //--------------------------------
+// // 推送訊息
+// //--------------------------------
+// function PushMsg(id) {
+//     let allUsers = id;
+//     for (var i = 0; i < allUsers.length; i++) {
+//         Admin.AdminMessengePushJdge(allUsers[i]).then(data => {
+//             if (data == -1) {
+//                 console.log('觸發-1');
+//             }
+//             else if (data == -9) {
+//                 console.log('處發-9');
+//             }
+//             else {
+//                 console.log('foreach');
+//                 data.forEach(item => {
+//                     //當下時間＃＃
+//                     var DateTime = new Date();
+//                     CurrentTime(DateTime);
+//                     //處理newDate()時間格式
+//                     function CurrentTime(strDate) {
+//                         var date = new Date(strDate);
+//                         var y = date.getFullYear();
+//                         console.log('y');
+//                         console.log(y);
+//                         var m = date.getMonth() + 1;
+//                         m = m < 10 ? ('0' + m) : m;
+//                         console.log('m');
+//                         console.log(m);
+//                         var d = date.getDate();
+//                         d = d < 10 ? ('0' + d) : d;
+//                         console.log('d');
+//                         console.log(d);
+//                         var h = date.getHours();
+//                         h = h < 10 ? ('0' + h) : h;
+//                         var minute = date.getMinutes();
+//                         minute = minute < 10 ? ('0' + minute) : minute;
+//                         var s = date.getSeconds();
+//                         s = s < 10 ? ('0' + s) : s;
+//                         var str = y + "-" + m + "-" + d + "T" + h + ":" + minute + ":" + s + '.000Z';
+//                         console.log('str'+str)
+//                         timeFn(item.adminpush_enddate, str)
+//                     };
+//                     //判斷是否在到期3小時內，每1小時推播一次
+//                     function timeFn(d1, CurrentTime) {//傳入處理好的時間
+//                         var dateBegin = new Date(d1);//傳入參數
+//                         var dateEnd = new Date(CurrentTime);
+//                         console.log('判斷相差時間');
+//                         console.log(dateBegin);
+//                         console.log(dateEnd);
+//                         var dateDiff = dateBegin.getTime() - dateEnd.getTime();//时间差的毫秒數
+//                         console.log('dateDiff'+dateDiff);
+//                         var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天數
+//                         console.log('dayDiff'+dayDiff);
+//                         var leave1 = dateDiff % (24 * 3600 * 1000)    //计算天數後剩餘的毫秒數
+//                         console.log('leave1'+leave1);
+//                         var hours = Math.floor(leave1 / (3600 * 1000))//计算出小時數
+//                         console.log('hours'+hours);
+//                         //计算相差分鐘數
+//                         var leave2 = leave1 % (3600 * 1000)    //计算小时數後剩餘毫秒數
+//                         var minutes = Math.floor(leave2 / (60 * 1000))//计算相差分鐘數
+//                         //计算相差秒數
+//                         var leave3 = leave2 % (60 * 1000)      //计算分鐘數後剩餘毫秒數
+//                         var seconds = Math.round(leave3 / 1000)
+//                         console.log(" 相差 " + dayDiff + "天 " + hours + "小時" + minutes + "分鐘" + seconds + " 秒")
+//                         // if (hours < 3 && hours >= 0) {
+//                         //     console.log('進行推播')
+//                         //     BotPushMsg()
+//                         //     var timer2;
+//                         //     function BotPushMsg(){
+//                         //         clearTimeout(timer2);
+//                         //         bot.push(item.user_id,'組長說：'+item.adminpush_content+'\n'+'到期時間'+item.adminpush_enddate);
+//                         //     }
+//                         //     timer2 = setInterval(BotPushMsg, 1000*60*60);
+//                         // }
+//                         console.log(" 相差 " + dayDiff + "天 " + hours + "小時" + minutes + "分鐘" + seconds + " 秒")
+//                     }
+//                     // bot.push(item.user_id,'組長說：'+item.adminpush_content+'\n'+'到期時間'+item.adminpush_enddate);
+//                 })
+//             }
+//         })
+//     }
+// }
 
-process.env.TZ = "America/New_York";
+process.env.TZ = "Asia/Shanghai";
 Date.prototype.TimeZone = new Map([
     ['Europe/London',0],
     ['Asia/Shanghai',-8],
