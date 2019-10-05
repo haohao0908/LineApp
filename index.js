c4a77998b508c0b53f85bc96fd410a06c87ba696
haohao0908 <N1056444@ntub.edu.tw>
2019-10-05 16:06:08 +0800

    Add console

--- index.js
+++ index.js
@@ -104,27 +104,7 @@ let letselectUser= setInterval(function(){
 //--------------------------------
 //推送訊息
 //--------------------------------
-// let push=setInterval(function PushMsg(id){
-//     let allUsers = id;
-//     for (var i = 0; i < allUsers.length; i++) {
-//         Admin.AdminMessengePushJdge(allUsers[i]).then(data => {
-//             if (data == -1) {
-//                 console.log('觸發-1');
-//             }
-//             else if (data == -9) {
-//                 console.log('處發-9');
-//             }
-//             else {
-//                 console.log('foreach');
-//                 data.forEach(item => {
-//                     console.log(item.adminpush_enddate);
-//                     bot.push(item.user_id, '組長說：' + item.adminpush_content + '\n' + '到期時間' + item.adminpush_enddate);
-//                 })
-//             }
-//         })
-//     }
-// },36000000)
-function PushMsg(id) {
+let push=setInterval(function PushMsg(id){
     let allUsers = id;
     for (var i = 0; i < allUsers.length; i++) {
         Admin.AdminMessengePushJdge(allUsers[i]).then(data => {
@@ -143,7 +123,27 @@ function PushMsg(id) {
             }
         })
     }
-}
+},36000000)
+// function PushMsg(id) {
+//     let allUsers = id;
+//     for (var i = 0; i < allUsers.length; i++) {
+//         Admin.AdminMessengePushJdge(allUsers[i]).then(data => {
+//             if (data == -1) {
+//                 console.log('觸發-1');
+//             }
+//             else if (data == -9) {
+//                 console.log('處發-9');
+//             }
+//             else {
+//                 console.log('foreach');
+//                 data.forEach(item => {
+//                     console.log(item.adminpush_enddate);
+//                     bot.push(item.user_id, '組長說：' + item.adminpush_content + '\n' + '到期時間' + item.adminpush_enddate);
+//                 })
+//             }
+//         })
+//     }
+// }
 
 //----------------------------------------
 // 建立一個網站應用程式app
