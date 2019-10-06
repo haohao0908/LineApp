ae54888bd5873b0a9d54c63f4ba05f306107050d
haohao0908 <N1056444@ntub.edu.tw>
2019-10-07 00:03:22 +0800

    edit push

--- utility/Admin.js
+++ utility/Admin.js
@@ -72,7 +72,7 @@ var AdminMessengePushJdge = async function(){
     //存放結果
     let result;
     //讀取資料庫
-    await query("SELECT mem.user_id,admin.adminpush_content,admin.adminpush_enddate FROM teammember as mem INNER JOIN adminpush as admin ON (admin.project_id=mem.project_id) where mem.user_id='U32851128a5210964818860dd9204b886'")
+    await query("SELECT mem.user_id,admin.adminpush_content,admin.adminpush_enddate FROM teammember as mem INNER JOIN adminpush as admin ON (admin.project_id=mem.project_id)")
         .then((data) => {
             if(data.rows.length > 0){
                 result = data.rows;  //學生資料(物件)
