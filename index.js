d6badff585ab5eb228107587015936f56e2c52fd
108406 <ntub108406@gmail.com>
2019-10-13 19:29:09 +0800

    push

--- index.js
+++ index.js
@@ -62,68 +62,70 @@ bot.on('unfollow', function (event) {
     });
 });
 bot.on('message', function (event) {
-    event.reply(
+    pushWorkText=[];
+    pushWorkText.push(
         {
             "type": "flex",
             "altText": "組長提醒：",
-            "contents":                {
+            "contents": {
                 "type": "bubble",
                 "body": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "spacing": "md",
-                  "contents": [
-                    {
-                      "type": "box",
-                      "layout": "vertical",
-                      "contents": [
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "md",
+                    "contents": [
                         {
-                          "type": "text",
-                          "text": "組長提醒",
-                          "align": "center",
-                          "size": "xxl",
-                          "weight": "bold"
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "組長提醒",
+                                    "align": "center",
+                                    "size": "xxl",
+                                    "weight": "bold"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": 'test',
+                                    "wrap": true,
+                                    "weight": "bold",
+                                    "margin": "lg"
+                                }
+                            ]
                         },
                         {
-                          "type": "text",
-                          "text": "test",
-                          "wrap": true,
-                          "weight": "bold",
-                          "margin": "lg"
+                            "type": "separator"
+                        },
+                        {
+                            "type": "text",
+                            "text": '結束時間:',
+                            "wrap": true,
+                            "weight": "bold",
+                            "margin": "lg"
                         }
-                      ]
-                    },
-                    {
-                      "type": "separator"
-                    },
-                    {
-                      "type": "text",
-                      "text": '結束時間:',
-                      "wrap": true,
-                      "weight": "bold",
-                      "margin": "lg"
-                    }
-                  ]
+                    ]
                 }
-              } 
-          }
+            }
+        }
     )
+    bot.push('U32851128a5210964818860dd9204b886',pushWorkText);
 })
 //========================================
 // 機器人接受訊息的處理
 //========================================
-bot.on('message', function(event) {
+bot.on('message', function (event) {
     event.source.profile().then(
         function (profile) {
-            if(event.message.text=="#我的計畫" || event.message.text=="#我的計劃" || event.message.text=="#我的專案"){
-                Messenge.MessengeSelectSearch(profile.userId).then(data =>{
-                    if(data==-1){
+            if (event.message.text == "#我的計畫" || event.message.text == "#我的計劃" || event.message.text == "#我的專案") {
+                Messenge.MessengeSelectSearch(profile.userId).then(data => {
+                    if (data == -1) {
                         event.reply('您可能還沒加入任何計畫哦！')
                     }
-                    else{
+                    else {
                         let pushWorkText = [];
-                        for(let i=0; i<data.length; i++){
-                            if(data[i].linebotpush){
+                        for (let i = 0; i < data.length; i++) {
+                            if (data[i].linebotpush) {
                                 pushWorkText.push({
                                     "title": "【您的計畫】",
                                     "text": data[i].project_name,
@@ -134,7 +136,7 @@ bot.on('message', function(event) {
                                             "uri": "https://zh.wikipedia.org/wiki/星夜"
                                         }
                                     ]
-                                });    
+                                });
                             }
                         }
                         event.reply({
@@ -142,65 +144,65 @@ bot.on('message', function(event) {
                             "altText": "這是一個輪播樣板",
                             "template": {
                                 "type": "carousel",
-                                "columns":pushWorkText
-                            }, 
-                        });  
+                                "columns": pushWorkText
+                            },
+                        });
                     }
                 })
             }
-            if(event.message.text=="#我的工作"){
-                Messenge.WorkSelectSearch(profile.userId).then(data =>{
+            if (event.message.text == "#我的工作") {
+                Messenge.WorkSelectSearch(profile.userId).then(data => {
                     console.log('index');
                     console.log(data);
-                    if(data==-1){
+                    if (data == -1) {
                         console.log('come')
                         event.reply('您可能還沒任何工作哦！');
                     }
-                    else{
+                    else {
                         event.reply('『以下是您的工作』');
-                        for(let i=0; i<data.length; i++){
+                        for (let i = 0; i < data.length; i++) {
                             let pushWorkText = '';
-                            if(data[i].work_hint){
-                                pushWorkText ='【'+data[i].work_title+'】';
+                            if (data[i].work_hint) {
+                                pushWorkText = '【' + data[i].work_title + '】';
                                 bot.push(profile.userId, [pushWorkText]);
                             }
                         }
                     }
                 })
             }
-            if(event.message.text=="#快到期計畫" || event.message.text=="#快到期計劃" || event.message.text=="##快到期專案" ){
-                Messenge.MessengeSelectSearch(profile.userId).then(data =>{
+            if (event.message.text == "#快到期計畫" || event.message.text == "#快到期計劃" || event.message.text == "##快到期專案") {
+                Messenge.MessengeSelectSearch(profile.userId).then(data => {
                     console.log('index');
                     console.log(data);
-                    if(data==-1){
+                    if (data == -1) {
                         event.reply('您可能還沒任何計畫快到期哦！');
                     }
-                    else{
+                    else {
                         let pushWorkText = [];
-                        for(let i=0; i<data.length; i++){
+                        for (let i = 0; i < data.length; i++) {
                             // let pushWorkText = '';
-                            if(data[i].linebotpush){
+                            if (data[i].linebotpush) {
                                 var dateBegin = new Date(data[i].project_enddate);//将-转化为/，使用new Date
                                 console.log(dateBegin);
                                 var dateEnd = new Date(Date.now() + (8 * 60 * 60 * 1000));//获取当前时间
                                 console.log(dateEnd);
                                 var dateDiff = dateBegin.getTime() - dateEnd.getTime();//时间差的毫秒数
                                 var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
-                                var leave1=dateDiff%(24*3600*1000)    //计算天数后剩余的毫秒数
-                                var hours=Math.floor(leave1/(3600*1000))//计算出小时数
+                                var leave1 = dateDiff % (24 * 3600 * 1000)    //计算天数后剩余的毫秒数
+                                var hours = Math.floor(leave1 / (3600 * 1000))//计算出小时数
                                 //计算相差分钟数
-                                var leave2=leave1%(3600*1000)    //计算小时数后剩余的毫秒数
-                                var minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
+                                var leave2 = leave1 % (3600 * 1000)    //计算小时数后剩余的毫秒数
+                                var minutes = Math.floor(leave2 / (60 * 1000))//计算相差分钟数
                                 //计算相差秒数
-                                var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
-                                var seconds=Math.round(leave3/1000)
-                                console.log(" 相差 "+dayDiff+"天 "+hours+"小时 "+minutes+" 分钟"+seconds+" 秒")
-                                if(hours<5 && hours>=0){
-                                    date=myFunction.SeparateDate(data[i].project_enddate)
+                                var leave3 = leave2 % (60 * 1000)      //计算分钟数后剩余的毫秒数
+                                var seconds = Math.round(leave3 / 1000)
+                                console.log(" 相差 " + dayDiff + "天 " + hours + "小时 " + minutes + " 分钟" + seconds + " 秒")
+                                if (hours < 5 && hours >= 0) {
+                                    date = myFunction.SeparateDate(data[i].project_enddate)
                                     pushWorkText.push({
                                         "title": data[i].project_name,
-                                        "text": '結束時間:'+date[0] + '/' + date[1] + '/' + date[2] + ' ' +
-                                        date[3] + ':' + date[4] + ':' + date[5],
+                                        "text": '結束時間:' + date[0] + '/' + date[1] + '/' + date[2] + ' ' +
+                                            date[3] + ':' + date[4] + ':' + date[5],
                                         "actions": [
                                             {
                                                 "type": "uri",
@@ -208,7 +210,7 @@ bot.on('message', function(event) {
                                                 "uri": "https://zh.wikipedia.org/wiki/星夜"
                                             }
                                         ]
-                                    });    
+                                    });
                                     // pushWorkText ='專案名稱'+'\n'+
                                     //                 +'〖'+ data[i].project_name +'】'+'\n'+
                                     //                 '結束時間:'+date[0] + '/' + date[1] + '/' + date[2] + ' ' +
@@ -222,14 +224,14 @@ bot.on('message', function(event) {
                             "altText": "這是一個輪播樣板",
                             "template": {
                                 "type": "carousel",
-                                "columns":pushWorkText
+                                "columns": pushWorkText
                             },
                             "imageAspectRatio": "rectangle",
-                            "imageSize": "cover"    
-                        });  
+                            "imageSize": "cover"
+                        });
                     }
                 })
-            }	
+            }
         }
     );
 });
@@ -320,8 +322,53 @@ let push = setInterval(function () {
         if (AdminPushMessage_1h || AdminPushMessage_3h || AdminPushMessage_5h) {
             console.log('可以推波囉 正確進入了');
             pushWorkText.push(
-
-              )
+                {
+                    "type": "flex",
+                    "altText": "組長提醒：",
+                    "contents": {
+                        "type": "bubble",
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "md",
+                            "contents": [
+                                {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "組長提醒",
+                                            "align": "center",
+                                            "size": "xxl",
+                                            "weight": "bold"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": allWorkData[allDataIndex].adminpush_content,
+                                            "wrap": true,
+                                            "weight": "bold",
+                                            "margin": "lg"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "separator"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": '結束時間:' +
+                                        adminpush_enddate[0] + '/' + adminpush_enddate[1] + '/' + adminpush_enddate[2] + ' ' +
+                                        adminpush_enddate[3] + ':' + adminpush_enddate[4] + ':' + adminpush_enddate[5],
+                                    "wrap": true,
+                                    "weight": "bold",
+                                    "margin": "lg"
+                                }
+                            ]
+                        }
+                    }
+                }
+            )
             // pushWorkText =
             //     '組長提醒事項' + '\n' + '【' + allWorkData[allDataIndex].adminpush_content + '】' + '\n' + '結束時間:' +
             //     adminpush_enddate[0] + '/' + adminpush_enddate[1] + '/' + adminpush_enddate[2] + ' ' +
@@ -330,11 +377,11 @@ let push = setInterval(function () {
                 console.log('here');
                 console.log(pushWorkText);
                 userId = allWorkData[allDataIndex].user_id;
-                bot.push(userId,{
+                bot.push(userId, {
                     "type": "flex",
                     "altText": "組長提醒：",
-                    "contents":pushWorkText 
-                  });
+                    "contents": pushWorkText
+                });
             }
         }
     }
