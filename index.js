8377da68d0ae053e7227d57213f04062bc411620
108406 <ntub108406@gmail.com>
2019-10-13 11:32:29 +0800

    push

--- index.js
+++ index.js
@@ -67,20 +67,43 @@ bot.on('message', function (event) {
         "altText": "this is a flex message",
         "contents": {
             "type": "bubble",
-            "body": {
+            "header": {
                 "type": "box",
                 "layout": "vertical",
                 "contents": [
-                    {
-                        "type": "text",
-                        "text": "hello"
-                    },
-                    {
-                        "type": "text",
-                        "text": "world"
-                    }
+                  {
+                    "type": "text",
+                    "text": "Header text"
+                  }
                 ]
-            }
+              },
+              "hero": {
+                "type": "image",
+                "url": "https://example.com/flex/images/image.jpg"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Body text"
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Footer text"
+                  }
+                ]
+              },
+              "styles": {
+                "comment": "See the example of a bubble style object"
+              },
         }
     })
 })
